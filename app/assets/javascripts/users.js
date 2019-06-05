158f351b8cbe4abaaf3447300b399e92f1a4dff0
FionaDL <github email address>
2019-06-05 10:50:59 +0200

    added chain for id

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -5,22 +5,27 @@ $(document).ready(function() {
 });
 
 function eventListeners() {
-  $('button#current-challenge-button').on('click', () => {
-
-    let id = $(this).data("id")
+  $('button#current-challenge-button').on('click', (e) => {
+    e.preventDefault()
+    let id = (e.currentTarget.dataset.id)
     getCurrentChallenges(id)
   })
 
-  $('button#expired-challenge-button').on('click', () => {
-
-    let id = $(this).data("id")
-    getCurrentChallenges(id)
+  $('button#expired-challenge-button').on('click', (e) => {
+    e.preventDefault()
+    let id = (e.currentTarget.dataset.id)
+    getExpiredChallenges(id)
   })
-
 }
 
 function getCurrentChallenges(id) {
-  $.get("/users/" + 66 + ".json", function(data) {
+  $.get("/users/" + id + ".json", function(data) {
+    console.log(data)
+  })
+}
+
+function getExpiredChallenges(id) {
+  $.get("/users/" + id + ".json", function(data) {
     console.log(data)
   })
 }
