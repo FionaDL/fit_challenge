3dde19115e833c2edafffdb247a3cdda6a146371
FionaDL <github email address>
2019-06-08 16:57:29 +0200

    adding more info

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -45,8 +45,16 @@ function appendCurrentChallenges(data) {
 function addMoreInfoListener() {
   $('button.more-info').on('click', (e) => {
     e.preventDefault()
+    let user = (e.currentTarget.dataset.user)
     let id = (e.currentTarget.dataset.id)
-    getMoreInfo()
+    getMoreInfo(user)
+  })
+}
+
+function getMoreInfo(user) {
+  $.get("/users/" + user + ".json", function(data) {
+    console.log(data)
+    appendMoreInfo(data)
   })
 }
 
@@ -54,6 +62,19 @@ function getExpiredChallenges(id) {
   $.get("/users/" + id + ".json", function(data) {
     console.log(data)
   })
+}
+
+function appendMoreInfo(data) {
+  let challengeArray = data.current_challenges
+  console.log(challengeArray)
+
+  challengeArray => {
+    let newChallenge = new Challenge(challenge)
+    console.log(newChallenge.formatChallenge())
+    let challengeHtml = newChallenge.formatChallenge()
+    $("#current-challenges").append(challengeHtml)
+    addMoreInfoListener()
+   })
 }
 
 
@@ -66,12 +87,13 @@ class Challenge {
     this.workouts_needed = challenge.workouts_needed
     this.reward = challenge.reward
     this.notes = challenge.notes
+    this.user_id = challenge.user_id
    }
   }
 
   Challenge.prototype.formatChallenge = function(){
      return (`
        <h2>Name: ${this.name}</h2>
-       <button class="more-info" data-id="${this.id}">More about this challenge...</button>
+       <button class="more-info" data-id="${this.id}" data-user="${this.user_id}">More about this challenge...</button>
        `)
   }
