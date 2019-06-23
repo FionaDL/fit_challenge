2fbf3e4c1f436e9bd98fca6f8e5ea2f471869a29
FionaDL <github email address>
2019-06-23 23:21:02 +0200

    fixed signout problem

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
  get "/signin", to: "sessions#new"
  post "/signin", to: "sessions#create"
- delete "/signout", to: "sessions#destroy"
+ get "/signout", to: "sessions#destroy"
 
  get '/auth/github/callback' => 'sessions#create'
 
